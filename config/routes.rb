f1f8cc2e5aa83ed9618b344089879fe24c9e66cd
Daniel Silverman <daniel.aa.silverman@gmail.com>
2021-05-12 15:46:09 -0400

    Reviews added and implemented on restaurant show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, except: [:destroy, :update, :edit] do
   # add sub-resources after lunch!
-    resources :reviews, only: [:new, :index]
+    resources :reviews, only: [:new, :create, :index]
   end
 end
